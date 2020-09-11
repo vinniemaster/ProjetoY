82056eacc4e2986cba01092a3b546c44171f8081
Vinicius <vinilcoelho_25@hotmail.com>
2020-09-11 15:46:24 -0300

    Atualização do Menu

---
+++ menu.py
@@ -0,0 +1,23 @@
+
+continua = 1
+while (continua == 1):
+    escolha_str = input('ESTOQUE MERCADO\nEscolha sua opção:\n1-Cadastro\n2-Consulta\n3-Sair\n')
+    escolha = int(escolha_str)
+
+    if escolha == 1:
+        try:
+#           class/function()
+        except (ValueError, RuntimeError, TypeError, NameError):
+#           class/function()
+        pass
+    elif escolha == 2:
+        try:
+#           class/function()
+            print('Produto Cadastrado com sucesso')
+        except (RuntimeError, TypeError, NameError, OSError):
+            print('Sem produtos cadastrados')
+        pass
+    elif escolha == 3:
+        break
+    else:
+        print('opção inválida!')
