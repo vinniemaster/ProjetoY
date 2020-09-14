001471fa813e3a1ce81a659882db92caa1a295a5
Felipe <luis.delmondes@outlook.com>
2020-09-14 16:17:36 -0300

    TESTEV2

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ while True:
     try:
         print('***Controle de Estoque***')
         opt = int(input('1 - Menu Cadastro\n 2 - Sair\nEscolha: '))
+
+        if (opt == 1):
+            estoque.cadastro_prod()
+            print('Produto cadastrado com sucesso !!')
+        
+        elif (opt == 2):
+            estoque.consulta_prod()
+            clear()
+            break
